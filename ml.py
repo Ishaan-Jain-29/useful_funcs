e32562504453036ef8d2e429f050b216858da21a
Ishaan-Jain-29 <96511416+Ishaan-Jain-29@users.noreply.github.com>
2025-02-28 11:22:20 +0530

    Create ml.py
    added plot_preds()

---
+++ ml.py
@@ -0,0 +1,7 @@
+def plot_preds(training_data, training_labels, testing_data, testing_labels, preds):
+  plt.figure(figsize=(10, 7))
+  plt.scatter(training_data, training_labels, c="g", label="Training Data")
+  plt.scatter(testing_data, testing_labels, c="b", label="Testing Data")
+  plt.scatter(testing_data, preds, c="r", label="Predictions")
+  plt.legend()
+  plt.show()
